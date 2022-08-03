9c7f6c16e075e978c41299603fd64deab0103c9d
Walkyst <41806921+Walkyst@users.noreply.github.com>
2022-08-04 02:57:10 +0300

    Add 4009 code

--- core/src/main/java/moe/kyokobot/koe/gateway/AbstractMediaGatewayConnection.java
+++ core/src/main/java/moe/kyokobot/koe/gateway/AbstractMediaGatewayConnection.java
@@ -126,6 +126,8 @@ public abstract class AbstractMediaGatewayConnection implements MediaGatewayConn
             if (connectAttempt <= 3) {
                 switch (code) {
                     //case 4006: // Session no longer valid
+                    case 4009: // Session timeout
+                        resumable = false;
                     case 1001: // Going away or CloudFlare WebSocket proxy restarting
                     case 1006: // Abnormal closure
                     case 4000: // Internal error
